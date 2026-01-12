eca652362651ccc3fe7118ff5864acae82d5361e
Mehdi-Zafar <mehdizaffar0@gmail.com>
2026-01-13 00:22:45 +0500

    Moved auth client to env

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.AUTHCLIENT_BASE_URL,
 });
 
 // Export the hooks and methods for easier importing
